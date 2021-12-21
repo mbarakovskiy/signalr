953b9378a477bc2f3db7e319785694f52c61c786
Mikhail Barakovskiy <mbarakovskiy@gmail.com>
2021-12-21 18:05:45 +0500

    Done

---
+++ BadNews/Controllers/CommentsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using BadNews.Models.Comments;
+using BadNews.Repositories.Comments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers
+{
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentsRepository commentsRepository;
+
+        public CommentsController(CommentsRepository commentsRepository)
+        {
+            this.commentsRepository = commentsRepository;
+        }
+
+        // GET
+        [HttpGet("api/news/{id}/comments")]
+        public ActionResult<CommentsDto> GetCommentsForNews(Guid newsId)
+        {
+            // TODO
+            var comments = commentsRepository.GetComments(newsId);
+            var commentsDto = new CommentsDto
+            {
+                NewsId = newsId,
+                Comments = comments
+            };
+            return commentsDto;
+        }
+    }
+}
---
+++ BadNews/Models/Comments/CommentsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using BadNews.Repositories.Comments;
+
+namespace BadNews.Models.Comments
+{
+    public class CommentsDto
+    {
+        public Guid NewsId { get; set; }
+
+        public IReadOnlyCollection<Comment> Comments { get; set; }
+    }
+}
